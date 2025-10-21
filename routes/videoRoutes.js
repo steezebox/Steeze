57855d2bf6f1ab35c435b21a57e76c1c097cdd53
steezebox <mukharrimabdulhadi@gmail.com>
2025-10-21 21:37:17 +0100

    Update videoRoutes.js

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { uploadVideo, unlockVideo } = require('../controllers/videoController');
+
+router.post('/upload', uploadVideo);
+router.post('/unlock', unlockVideo);
+
+module.exports = router;
 
